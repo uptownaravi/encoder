b21bd569c50be645d8257d9f3d7d76dd59839ded
Harish Aravindan <harisharavindan92@gmail.com>
2021-09-25 22:37:22 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+#with open("readme.md", "r") as fh:
+#    long_description = fh.read()
+
+setup(
+    name='encodepacakge',  
+    version='0.0.1',
+    author="uptownaravi",
+    author_email="uptownaravi@gmail.com",
+    description="encode string with given template",
+    #long_description_content_type="text/markdown",
+    url="https://github.com/uptownaravi/encoder",
+    package_dir={"": "encoder"},
+    packages=find_packages(where="encoder"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+ )
