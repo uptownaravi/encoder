54db3146fd2bd2a594fc259978960a764b88abe0
Harish Aravindan <harisharavindan92@gmail.com>
2021-09-25 22:40:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='encodepacakge',  
-    version='0.0.1',
+    version='0.0.3',
     author="uptownaravi",
     author_email="uptownaravi@gmail.com",
     description="encode string with given template",
